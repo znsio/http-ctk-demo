af8d59c037389a366118e843c5810084274bbd0a
Joel Rosario <cirquitz@gmail.com>
2024-08-05 21:26:55 +0530

    Updated the ctk to run via docker

--- TMF621-HTTP-CTKv5/ctk.sh
+++ TMF621-HTTP-CTKv5/ctk.sh
@@ -2,7 +2,10 @@
 
 specmatic="$DEFAULT_SPECMATIC_PATH"
 
-EXTENSIBLE_SCHEMA=true specmatic test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://localhosT:8621/tmf-api/troubleTicket/v5'
+ docker run -v "./specmatic.yaml:/usr/src/app/specmatic.yaml" znsio/specmatic:2.0.7 test --testBaseURL='http://localhost:8621/tmf-api/troubleTicket/v5'
+
+
+EXTENSIBLE_SCHEMA=true specmatic test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://localhost:8621/tmf-api/troubleTicket/v5'
 
 rm -rf ./SEND_ME
 mv ./reports/specmatic/html SEND_ME
