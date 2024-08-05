1db7689747fbf97ca26067a204bd9045c95f2a74
Joel Rosario <cirquitz@gmail.com>
2024-08-05 22:06:27 +0530

    Updated the specmatic.yaml config file and omitted some paths

--- TMF621-HTTP-CTKv5/ctk.sh
+++ TMF621-HTTP-CTKv5/ctk.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-specmatic="$DEFAULT_SPECMATIC_PATH"
-
 rm -rf ./SEND_ME
 
-docker run -e EXTENSIBLE_SCHEMA=true -v './SEND_ME:/usr/src/app/reports/specmatic/html:rw' -v "./specmatic.yaml:/usr/src/app/specmatic.yaml" znsio/specmatic:2.0.7 test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://host.docker.internal:8621/tmf-api/troubleTicket/v5'
+docker run --rm -e EXTENSIBLE_SCHEMA=true -v './SEND_ME:/usr/src/app/reports/specmatic/html:rw' -v "./specmatic.yaml:/usr/src/app/specmatic.yaml" znsio/specmatic:2.0.7 test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://host.docker.internal:8621/tmf-api/troubleTicket/v5'
 
-
-# EXTENSIBLE_SCHEMA=true specmatic test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://localhost:8621/tmf-api/troubleTicket/v5'
-
-
-# mv ./reports/specmatic/html SEND_ME
-# rm -rf ./reports
