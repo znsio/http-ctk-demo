92c4e32bc280fe1004ebb1d8b9248c3253c7c828
Joel Rosario <cirquitz@gmail.com>
2024-08-06 12:37:52 +0530

    Run contract tests with the specmatic generative stub

--- TMF621-HTTP-CTKv5/ctk.sh
+++ TMF621-HTTP-CTKv5/ctk.sh
@@ -2,5 +2,5 @@
 
 rm -rf ./SEND_ME
 
-docker run --rm -e EXTENSIBLE_SCHEMA=true -v './SEND_ME:/usr/src/app/reports/specmatic/html:rw' -v "./specmatic.yaml:/usr/src/app/specmatic.yaml" znsio/specmatic:2.0.7 test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202,-> 4,->5' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://host.docker.internal:8621/tmf-api/troubleTicket/v5'
+docker run --rm -e MAX_TEST_REQUEST_COMBINATIONS=5 -e EXTENSIBLE_SCHEMA=true -v './SEND_ME:/usr/src/app/reports/specmatic/html:rw' -v "./specmatic.yaml:/usr/src/app/specmatic.yaml" znsio/specmatic:2.0.7 test --filter-not-name '/hub,/listener,PATCH,DELETE,-> 202' --filter-name '/troubleTicket ->,/troubleTicket/(id:string) ->' --testBaseURL='http://host.docker.internal:8621/tmf-api/troubleTicket/v5'
 
